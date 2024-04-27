2638f78e6d736b519f0d26256c99f0aa8cb60852
SHURUP2008 <SHURUP2008@bk.ru>
2024-04-27 15:01:44 +0300

    Добавьте файлы проекта.

---
+++ Project10-test/FileName.cpp
@@ -0,0 +1,32 @@
+#include "mathFunc.h"
+#include <iostream>
+
+int factorial(int x)
+{
+    if (x < 0)
+    {
+        return 1;
+    }
+    if (x == 1 || x == 0)
+    {
+        return 1;
+    }
+    return x * factorial(x - 1);
+}
+
+float divAtoB(float a, float b)
+{
+    return a / b;
+}
+
+bool isPositive(int x)
+{
+    return (x > 0 ? true : false);
+}
+
+
+std::string reverseStr(std::string str)
+{
+    std::reverse(str.begin(), str.end());
+    return str;
+}
